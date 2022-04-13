2002a734434b38ac8c6fc013358b0e3871a026e2
ragul-15 <srgamingkings@gmail.com>
2022-04-13 17:38:13 +0530

    git ignore created

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        GameObject object = (transform.position.x = 10);
+       
+    }
+}
